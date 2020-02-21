69b5964d075de9c52ae7fa0c4c3be74d3bdbb524
Nethelbert <gilblasse@gmail.com>
2020-02-21 15:02:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books')
+  fetch('http://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
   .then(json => renderBooks(json))
 }
